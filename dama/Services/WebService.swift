8150f1cc2e55ca12dd390ca368690b0a8dd3f9bb
hoonjoo94 <hoonjoo94@naver.com>
2023-02-12 03:23:43 +0900

    모든 메뉴 불러올 수 있도록 menus fetch 함수 구현

--- dama/Services/WebService.swift
+++ dama/Services/WebService.swift
@@ -9,13 +9,39 @@ import UIKit
 
 class WebService {
     static let shared = WebService()
-    private let baseUrl = ""
+    private let baseUrl = "http://localhost:3306"
+    let decoder = JSONDecoder()
+    let cache = NSCache<NSString, UIImage>()
     
-//    func fetchMenus() async throws -> [Menu] {
-//        return
-//    }
+    func fetchMenus() async throws -> [Menu] {
+        guard let url = URL(string: baseUrl + "/menu") else { throw ErrorMessages.InvalidUrl }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw ErrorMessages.InvalidResponse
+        }
+        
+        do {
+            let menus = try decoder.decode([Menu].self, from: data)
+            return menus
+        } catch {
+            throw ErrorMessages.InvalidData
+        }
+    }
     
-//    func downloadImage(imageUrl: String) async -> UIImage? {
-//        return
-//    }
+    func downloadImage(imageUrl: String) async -> UIImage? {
+        let cacheKey = NSString(string: imageUrl)
+        if let cachedImage = cache.object(forKey: cacheKey) { return cachedImage }
+        
+        guard let url = URL(string: imageUrl) else { return nil }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            guard let image = UIImage(data: data) else { return nil }
+            cache.setObject(image, forKey: cacheKey)
+            
+            return image
+        }
+        catch { return nil }
+    }
 }
