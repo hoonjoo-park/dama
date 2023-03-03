76b368220eca197c2b66bd7e9b0f4c21de96a194
hoonjoo94 <hoonjoo94@naver.com>
2023-03-04 01:57:56 +0900

    CartVC 기본적인 Conatiner View들에 대한 마크업 구현

--- dama/ViewControllers/CartVC.swift
+++ dama/ViewControllers/CartVC.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class CartVC: UIViewController {
+    let cartVM = CartViewModel.shared
+    
+    let bottomButton = DamaTextButton(frame: .zero)
+    let cartContainer = UIView(frame: .zero)
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = nil
+        
+        configureSubViews()
+        configureCartContainerUI()
+        configureUI()
     }
     
     
@@ -22,6 +31,44 @@ class CartVC: UIViewController {
         self.navigationController?.navigationBar.tintColor = DamaColors.black
         self.navigationController?.navigationBar.topItem?.title = ""
         view.backgroundColor = DamaColors.white
+    }
+    
+    
+    private func configureSubViews() {
+        [cartContainer, bottomButton].forEach { view.addSubview($0) }
+    }
+    
+    
+    private func configureCartContainerUI() {
+        cartContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        cartContainer.layer.cornerRadius = 15
+        cartContainer.layer.shadowColor = DamaColors.black.cgColor
+        cartContainer.layer.shadowOpacity = 0.25
+        cartContainer.layer.shadowOffset = CGSize(width: 0, height: 2)
+        cartContainer.layer.shadowRadius = 2
+        cartContainer.backgroundColor = DamaColors.lightGray
+        cartContainer.layer.masksToBounds = false
+        
+    }
+    
+    
+    private func configureUI() {
+        let padding:CGFloat = 25
+        
+        bottomButton.backgroundColor = DamaColors.orange
+        
+        NSLayoutConstraint.activate([
+            cartContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            cartContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            cartContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            cartContainer.heightAnchor.constraint(equalToConstant: 50),
+            
+            bottomButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            bottomButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            bottomButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            bottomButton.heightAnchor.constraint(equalToConstant: 60),
+        ])
     }
 
 }
