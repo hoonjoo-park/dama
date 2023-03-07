972de5f0a9355c761e1986c1b57ab1f52cfd4c5d
hoonjoo94 <hoonjoo94@naver.com>
2023-03-08 02:30:54 +0900

    주문완료 스크린 마크업 완료

--- dama/ViewControllers/OrderResultVC.swift
+++ dama/ViewControllers/OrderResultVC.swift
@@ -8,10 +8,75 @@
 import UIKit
 
 class OrderResultVC: UIViewController {
+    let boxImage = UIImageView(image: UIImage(named: "order-box"))
+    let boldTextLabel = DamaLabel(fontSize: 18, weight: .bold, color: DamaColors.black)
+    let regularTextLabel = DamaLabel(fontSize: 14, weight: .medium, color: DamaColors.lightGray)
+    let bottomButton = DamaTextButton(frame: .zero)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = DamaColors.orange
+        
+        configureBottomAction()
+        configureUI()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureNavigation()
+        view.backgroundColor = DamaColors.white
+    }
+    
+    
+    private func configureNavigation() {
+        let closeButton = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(dismissViewController))
+        closeButton.tintColor = DamaColors.black
+        
+        navigationItem.setHidesBackButton(true, animated: true)
+        navigationItem.setRightBarButton(closeButton, animated: true)
+    }
+    
+    
+    @objc func dismissViewController() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    private func configureBottomAction() {
+        bottomButton.addTarget(self, action: #selector(dismissViewController), for: .touchUpInside)
+    }
+    
+    
+    private func configureUI() {
+        let padding:CGFloat = 25
+        
+        [boxImage, boldTextLabel, regularTextLabel, bottomButton].forEach { [weak self] subView in
+            self?.view.addSubview(subView)
+        }
+        
+        boxImage.translatesAutoresizingMaskIntoConstraints = false
+        bottomButton.backgroundColor = DamaColors.orange
+        boldTextLabel.text = "주문이 완료되었습니다!"
+        bottomButton.setText("처음으로")
+        
+        NSLayoutConstraint.activate([
+            boxImage.widthAnchor.constraint(equalToConstant: 150),
+            boxImage.heightAnchor.constraint(equalToConstant: 150),
+            boxImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 190),
+            boxImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            boldTextLabel.topAnchor.constraint(equalTo: boxImage.bottomAnchor, constant: 54),
+            boldTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            regularTextLabel.topAnchor.constraint(equalTo: boldTextLabel.bottomAnchor, constant: 30),
+            regularTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            bottomButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            bottomButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            bottomButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            bottomButton.heightAnchor.constraint(equalToConstant: 60),
+        ])
     }
     
 }
